76dbd0d74f34ce653dac086f0c58f6e538cb20a9
Martin Evans <martinjosephevans@googlemail.com>
2020-04-04 14:13:44 +0100

    Use JSX files and styled the search box

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TeamsTable from './teamsTable.jsx';
+import MyNavBar from './myNavBar.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <MyNavBar/>                  
+    </div>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap/dist/react-bootstrap.js';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
